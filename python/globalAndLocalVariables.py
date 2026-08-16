017705ad1f02d50ef65234b347db438eba779557
Ramya_Achyuta <ramyasriachyuta@gmail.com>
2026-08-16 17:35:23 +0530

    GlobalAndLocalVariables.py

--- python/globalAndLocalVariables.py
+++ python/globalAndLocalVariables.py
@@ -3,21 +3,21 @@ a = 15 #global
 
 def doSomething():
     a = 10 #local
-    print("inside : ",a)
+    print("inside : ",a) #10
 doSomething()
-print("outsid : ",a)
+print("outsid : ",a) #15
 
 def doSomething():
     global a  # treats this a as global
     a = 10 #local (changes both local and global value to 10)
-    print("inside : ",a)
+    print("inside : ",a) # 10
 doSomething()
-print("outsid : ",a)
+print("outsid : ",a) # 10
 
 
 def doSomething():
     globals()['a']  = 10 # changes globally value
     a= 20  #consider this a is now local val and prints the value
-    print("inside : ",a)
+    print("inside : ",a) # 20
 doSomething()
-print("outside : ",a)
+print("outside : ",a) # 10
